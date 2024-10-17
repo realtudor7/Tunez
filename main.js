33b898ce296ae357de0e884122d7a8479185de3f
realtudor7 <tanasealexandru871@gmail.com>
2024-10-17 10:50:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+function manele() {
+    location.href = "manele.html"
+}
+
+function camion() {
+    var audio = new Audio('camion.mp3');
+    audio.play();
+}
+
+function diamante() {
+    var audio = new Audio('diamante.mp3');
+    audio.play();
+}
+
+function printesa() {
+    var audio = new Audio('printesa.mp3');
+    audio.play();
+}
+function zanamea() {
+    var audio = new Audio('zanamea.mp3');
+    audio.play();
+}
+
+function thomas() {
+    var audio = new Audio('thomas.mp3');
+    audio.play();
+}
+
+function phonk() {
+    location.href = "phonk.html"
+}
+
+function else1() {
+    location.href = "else1.html"
+}
+
+function jann() {
+    var audio = new Audio('jann.mp3')
+    audio.play();
+}
+
+function hottogo() {
+    var audio = new Audio('hottogo.mp3');
+    audio.play();
+}
+
+function dream() {
+    var audio = new Audio('speedrun.mp3');
+    audio.play();
+}
+
+function kahoot() {
+    var audio = new Audio('kahoot.mp3');
+    audio.play();
+}
+
+function bug() {
+    var audio = new Audio('strazile.mp3');
+    audio.play();
+}
+
+function fein() {
+    var audio = new Audio('fein.mp3');
+    audio.play();
+}
